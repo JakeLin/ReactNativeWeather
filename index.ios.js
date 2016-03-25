8fa2d8a20dc5a757f5893bade01da297773e7740
Jake Lin <JakeLinAu@gmail.com>
2016-03-26 10:20:25 +1100

    Use `NavigatorIOS` to contain `WeatherView` in main view

--- index.ios.js
+++ index.ios.js
@@ -8,24 +8,21 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS,
 } from 'react-native';
+
+var WeatherView = require('./src/WeatherView');
 
 class ReactNativeWeather extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native Weather!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: WeatherView,
+        }}/>
     );
   }
 }
@@ -33,19 +30,6 @@ class ReactNativeWeather extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
