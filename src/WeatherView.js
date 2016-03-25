3adb3b7ba3ea6850ed3eee33b33651e16d780e36
Jake Lin <JakeLinAu@gmail.com>
2016-03-26 10:20:44 +1100

    Add some layout in `WeatherView`

--- src/WeatherView.js
+++ src/WeatherView.js
@@ -10,19 +10,44 @@ import React, {
 class WeatherView extends Component {
   render() {
     return (
-      <View>
-        <Text style={Styles.icon}>Cloudy!!!Cloudy!!!Cloudy!!!Cloudy!!!Cloudy!!!Cloudy!!!</Text>
-      </View>
+      <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to React Native Weather haha!
+      </Text>
+      <Text style={styles.instructions}>
+        To get started, edit index.ios.js
+      </Text>
+      <Text style={styles.instructions}>
+        Press Cmd+R to reload,{'\n'}
+        Cmd+D or shake for dev menu
+      </Text>
+    </View>
     );
   }
 }
 
-const Styles = StyleSheet.create({
+const styles = StyleSheet.create({
   icon: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
 
